c50bb88544f95f04beca34a2e4ad9656d24cf5f7
Alona Harnyk <alyonagarnyk@gmail.com>
2023-09-01 09:37:47 +0300

    Revert "Hw05 avatars"

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,12 +1,10 @@
 const {validateBody, validateUpdateStatusContact} = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate")
-const upload = require("./upload")
 
 module.exports = {
     validateBody,
     isValidId,
     validateUpdateStatusContact,
     authenticate,
-    upload,
 }
--- models/user.js
+++ models/user.js
@@ -5,69 +5,53 @@ const Joi = require("joi");
 const emailRegexp = /[^\s@]+@[^\s@]+\.[^\s@]+/;
 
 const userSchema = new Schema(
- {
-  password: {
-   type: String,
-   required: [true, "Password is required"],
-   minlength: 6,
-  },
-  email: {
-   type: String,
-   required: [true, "Email is required"],
-   unique: true,
-   match: emailRegexp,
-  },
-  subscription: {
-   type: String,
-   enum: ["starter", "pro", "business"],
-   default: "starter",
-  },
-  token: {
-   type: String,
-   default: "",
-    },
-    avatarURL: {
+  {
+    password: {
       type: String,
-      required: true,
-  }
- },
- { versionKey: false, timestamps: true }
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
- password: Joi.string().min(6).required(),
- email: Joi.string().pattern(emailRegexp).required(),
- subscription: Joi.string(),
- token: Joi.string(),
+    password: Joi.string().min(6).required(),
+    email: Joi.string().pattern(emailRegexp).required(),
+    subscription: Joi.string(),
+    token: Joi.string(),
 });
 
 const loginSchema = Joi.object({
- password: Joi.string().min(6).required(),
- email: Joi.string().pattern(emailRegexp).required(),
-});
-
-const validSubscriptionValues = ["starter", "pro", "business"];
-const updateUserSubscriptionSchema = Joi.object({
- subscription: Joi.string()
-  .valid(...validSubscriptionValues)
-  .messages({
-   "any.required": "Subscription field is required",
-   "string.base": "Subscription field must be a string",
-  })
-  .required(),
+    password: Joi.string().min(6).required(),
+    email: Joi.string().pattern(emailRegexp).required(),
 });
 
 const schemas = {
- registerSchema,
- loginSchema,
- updateUserSubscriptionSchema,
-};
+    registerSchema,
+    loginSchema,
+}
 
 const User = model("user", userSchema);
 
 module.exports = {
- User,
- schemas,
-};
+    User,
+    schemas,
+}
